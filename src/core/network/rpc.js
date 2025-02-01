791cb850f8c48b5b41fdb7182739a2a9f53822bc
CryptoBoyz <160847170+SRCryptoBoyz@users.noreply.github.com>
2025-02-01 17:00:40 +0700

    Delete src directory

--- src/core/network/rpc.js
+++
@@ -1,6 +0,0 @@
-  export class RPC {
-    static ['CHAINID'] = 0xaa36a7;
-    static ['RPCURL'] = "https://ethereum-sepolia-rpc.publicnode.com";
-    static ["EXPLORER"] = 'https://sepolia.etherscan.io/';
-    static ['SYMBOL'] = 'ETH';
-  }
