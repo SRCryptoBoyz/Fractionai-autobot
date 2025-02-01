e60508f4e16942b84c6d708a32bd47b3c44d7021
CryptoBoyz <160847170+SRCryptoBoyz@users.noreply.github.com>
2025-02-01 17:00:20 +0700

    Delete app/config directory

--- app/config/proxy_list.js
+++
@@ -1,5 +0,0 @@
-export const proxyList = [
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-  ];
