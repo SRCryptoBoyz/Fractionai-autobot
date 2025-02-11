ad9bd658780aab70a5e11122715476bb4dee436f
CryptoBoyz <160847170+SRCryptoBoyz@users.noreply.github.com>
2025-02-11 09:29:07 +0700

    Update accounts.js

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -1,5 +1,11 @@
-export const privateKey = [
-"PK1",
-"PK2",
-"PK3",
-];
+/**
+ * Private key list file
+ * write your private key here like this
+ * export const privateKey = [
+ *   "PK1",
+ *   "PK2",
+ *   "PK3",
+ * ];
+ */
+
+export const privateKey = ["PK1", "PK2"];
